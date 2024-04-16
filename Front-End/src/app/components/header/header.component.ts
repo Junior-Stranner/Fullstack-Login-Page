af89c53dea2d4ea214e8f46f4fc49e418c304893
Junior-Stranner <Ju-stranner@outlook.com>
2024-04-16 11:42:23 -0300

    Now I'm improving scss and improving and implementing new things in html

--- Front-End/src/app/components/header/header.component.ts
+++ Front-End/src/app/components/header/header.component.ts
@@ -5,7 +5,7 @@ import { HomeComponent } from '../home/home.component';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [HomeComponent,HeaderComponent,NgOptimizedImage],
+  imports: [HeaderComponent,NgOptimizedImage],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
