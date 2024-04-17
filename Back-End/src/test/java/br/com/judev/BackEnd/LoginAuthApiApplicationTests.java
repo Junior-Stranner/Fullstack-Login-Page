353178d1d7c68c56084cb502b2e629031d2d3bf9
Junior-Stranner <Ju-stranner@outlook.com>
2024-04-17 09:52:11 -0300

    start with Back-end Project

---
+++ Back-End/src/test/java/br/com/judev/BackEnd/LoginAuthApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.judev.BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginAuthApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- Front-End/src/app/app.component.ts
+++ Front-End/src/app/app.component.ts
@@ -1,14 +1,25 @@
+// Importa o decorador 'Component' de '@angular/core', que é usado para definir componentes Angular.
 import { Component } from '@angular/core';
+// Importa 'RouterOutlet' de '@angular/router', que é usado para renderizar componentes quando a rota corresponde a um caminho específico.
 import { RouterOutlet } from '@angular/router';
+// Importa o componente 'HomeComponent' do arquivo './components/home/home.component', que será usado posteriormente.
 import { HomeComponent } from './components/home/home.component';
-
+// Define um novo componente Angular com o seletor 'app-root'.
 @Component({
   selector: 'app-root',
+  // Define 'standalone' como true, o que não é um atributo padrão em Angular. 
+  // Normalmente, não faz parte da configuração de componentes Angular.
   standalone: true,
+  // Importa o 'RouterOutlet', que não é um módulo Angular. 
+  // Normalmente, os módulos são importados no arquivo do módulo, não no componente.
   imports: [RouterOutlet],
+  // Define o template do componente, que será renderizado no lugar onde este componente for utilizado.
   templateUrl: './app.component.html',
+  // Define o estilo do componente a partir do arquivo './app.component.scss'.
   styleUrl: './app.component.scss'
 })
+// Define a classe AppComponent, que representa a lógica do componente.
 export class AppComponent {
+  // Define uma propriedade 'title' que armazena o título do aplicativo.
   title = 'landing-page';
 }
